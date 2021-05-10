0577d8a790f9e382a38338e4e7aac4ec21afb2e5
Ibrahim <79158349+ibrahimfayyaz26@users.noreply.github.com>
2021-05-10 19:13:36 +0500

    Add files via upload

---
+++ screens/Products/ProductMain.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProductMain = () => {
+  return (
+    <View>
+      <Text>Main Screen</Text>
+    </View>
+  );
+};
+
+export default ProductMain;
+
+const styles = StyleSheet.create({});
